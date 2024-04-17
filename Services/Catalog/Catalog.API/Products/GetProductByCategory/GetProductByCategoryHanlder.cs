b462ab114e25581c042140e0749bca355b7f5429
iman <dev.imansabet@gmail.com>
2024-04-17 21:56:27 +0330

    GetProductByCategory Handler

---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHanlder.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+
+public class GetProductByCategoryQueryHanlder(IDocumentSession session, ILogger<GetProductByCategoryQueryHanlder> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+        var products = await session.Query<Product>()
+            .Where(p=>p.Category.Contains(query.Category))
+            .ToListAsync();
+        return new GetProductByCategoryResult(products);
+    }
+}

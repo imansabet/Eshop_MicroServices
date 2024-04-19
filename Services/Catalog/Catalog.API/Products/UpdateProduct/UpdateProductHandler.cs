cb9826ff842dd915805d2c15b0004c342801f70f
iman <dev.imansabet@gmail.com>
2024-04-19 09:39:30 +0330

    DeleteProduct Handler

--- Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace Catalog.API.Products.UpdateProduct;
-public record UpdateProductCommand(Guid Id ,string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>
+public record UpdateProductCommand(Guid Id ,string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
 
 public record UpdateProductResult(bool IsSuccess);
 

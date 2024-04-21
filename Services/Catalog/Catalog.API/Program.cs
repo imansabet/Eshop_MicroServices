648361add866d312f667191b6b662670ed1972f4
iman <dev.imansabet@gmail.com>
2024-04-21 20:40:47 +0330

    HealthCheck for Catalog MicroServices with PostgresDB

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -21,10 +24,18 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+
 var app = builder.Build();
 
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 
-
+app.UseHealthChecks("/health",
+    new HealthCheckOptions 
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse   
+    });
 app.Run();

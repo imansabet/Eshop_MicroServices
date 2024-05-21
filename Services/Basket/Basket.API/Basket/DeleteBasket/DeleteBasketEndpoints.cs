7782cbbc58f89245afb4b99a4fe7a7ef7ce3f6ee
iman <dev.imansabet@gmail.com>
2024-05-21 18:40:46 +0330

    DeleteBasket Endpoint (Carter Minimal Api)

--- Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Basket.API.Basket.GetBasket;
+using Basket.API.Basket.Storebasket;
+
+namespace Basket.API.Basket.DeleteBasket;
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool  IsSuccess);
+
+public class DeleteBasketEndpoints : ICarterModule
 {
-    public class DeleteBasketEndpoints
+    public  void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+            var response = result.Adapt<DeleteBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeleteBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary(" Delete Basket")
+        .WithDescription("Delete Basket");
     }
 }

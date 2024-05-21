16d9595606a4eebca8d696c06095787ebd40c696
iman <dev.imansabet@gmail.com>
2024-05-21 19:21:06 +0330

    Repository Pattern : IBasketRepository

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,26 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//An assembly is the compiled output of your code, typically a DLL, but your EXE is also an assembly.
+//It's the smallest unit of deployment for any .NET project.
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
+
 //Add services to the container.
 var app = builder.Build();
 
 //Configure http request pipeline
-
+app.MapCarter();
 
 app.Run();

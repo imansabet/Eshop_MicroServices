9e3379e391038a938005e99f0e60069aa621fdf9
iman <dev.imansabet@gmail.com>
2024-05-21 18:35:30 +0330

    DeleteBasket feature handler

--- Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿using Basket.API.Basket.Storebasket;
+
+namespace Basket.API.Basket.DeleteBasket;
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess );
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
-    public class DeleteBasketHandler
+    public DeleteBasketCommandValidator()
     {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required.");
+    }
+}
+public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        //Todo : Delete basket from cache and db 
+        //session.delete<product>(coomand.id)
+        return new DeleteBasketResult(true);
     }
 }

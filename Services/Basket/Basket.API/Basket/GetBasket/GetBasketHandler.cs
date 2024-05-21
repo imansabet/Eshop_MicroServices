fc5c71334813aabffe06d4a70db544df4ae8d3ac
iman <dev.imansabet@gmail.com>
2024-05-21 17:48:48 +0330

    start GetBasket Feature Handler

--- Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
-    public class GetBasketHandler
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
+        // Todo : 
+        // Get basket from db
+        // basket = _repo.getbasket(request.username)
+        return new GetBasketResult(new ShoppingCart("Swn"));
     }
 }

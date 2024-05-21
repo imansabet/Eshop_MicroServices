fc0a90117dcc0d0f7274b4e0defc2f634f648f81
iman <dev.imansabet@gmail.com>
2024-05-21 18:30:15 +0330

    StoreBasket Endpoint (Carter minimal Api)

--- Services/Basket/Basket.API/Basket/Storebasket/StoreBasketEndpoints.cs
+++ Services/Basket/Basket.API/Basket/Storebasket/StoreBasketEndpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.Storebasket
+﻿
+using Basket.API.Basket.GetBasket;
+
+namespace Basket.API.Basket.Storebasket;
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoints : ICarterModule
 {
-    public class StoreBasketEndpoints
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<StoreBasketResponse>();
+            return Results.Created($"basket/{response.UserName}", response);
+        })
+        .WithName("CreateBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary(" Create Basket")
+        .WithDescription("Create Basket");
     }
 }

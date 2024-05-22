4aff1e6add040066e25ca59255b01a1273e71a17
iman <dev.imansabet@gmail.com>
2024-05-22 10:16:57 +0330

    fix 2 typo

--- Services/Basket/Basket.API/Models/ShoppingCartItem.cs
+++ Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -6,6 +6,6 @@
         public string Color { get; set; } = default!;
         public decimal Price { get; set; } = default!;
         public Guid ProductId { get; set; } = default!;
-        public string PrdouctName { get; set; } = default!;
+        public string ProductName { get; set; } = default!;
     }
 }

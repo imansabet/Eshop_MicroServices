f504f80cc2f84792c797463dfd04c3c40ddb7d73
iman <dev.imansabet@gmail.com>
2024-06-04 16:27:41 +0330

    Register EfCore  DbContext

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add Service to the container
@@ -8,6 +9,8 @@ builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices();
+
+
 
 var app = builder.Build();
 

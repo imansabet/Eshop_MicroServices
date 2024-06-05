4cb969d5066d15b510ebadf369f3806b64ff8090
iman <dev.imansabet@gmail.com>
2024-06-05 18:30:46 +0330

    Auto migrate efCore Entities

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,7 @@ using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add Service to the container
@@ -15,6 +16,12 @@ builder.Services
 var app = builder.Build();
 
 //configure the http request pipeline
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment()) 
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 
 app.Run();

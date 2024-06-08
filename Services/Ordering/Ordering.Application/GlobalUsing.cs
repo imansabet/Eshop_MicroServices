699822fde70e5a749f9ed2cd374a3f68077e936f
iman <dev.imansabet@gmail.com>
2024-06-08 17:47:25 +0330

    Develop Order Queries with CQRS & MediatR in Application Layer

--- Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -7,4 +7,5 @@ global using Ordering.Domain.ValueObjects;
 global using MediatR;
 global using Microsoft.Extensions.Logging;
 global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
 

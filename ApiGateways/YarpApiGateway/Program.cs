ce88a1f7b5f4d0be7d64e2c07c1baa11d199d2e9
iman <dev.imansabet@gmail.com>
2024-06-14 18:07:15 +0330

    implement YarpApiGateway

--- ApiGateways/YarpApiGateway/Program.cs
+++ ApiGateways/YarpApiGateway/Program.cs
@@ -1,10 +1,30 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the Container
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 
 var app = builder.Build();
 
 
 //Configure the Http request pipeline
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+
 
 app.Run();
